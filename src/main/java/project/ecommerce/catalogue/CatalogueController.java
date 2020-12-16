3e628f3bae9dcbd54c8b4771a033466b01ff7297
MTLantoine <mortelier.antoine@hotmail.fr>
2020-12-16 23:41:19 +0100

    🐛 catalogue display works

--- src/main/java/project/ecommerce/catalogue/CatalogueController.java
+++ src/main/java/project/ecommerce/catalogue/CatalogueController.java
@@ -15,8 +15,8 @@ public class CatalogueController {
     @Autowired
     private CatalogueRepository catalogueRepository;
 
-    // @GetMapping
-    // public Stream<Stock> getNotEmptyStock() {
-    //     return catalogueRepository.findById(1).get().getStock().stream().filter(stock -> stock.getQuantity() > 0);
-    // }
+    @GetMapping
+    public Stream<Stock> getNotEmptyStock() {
+        return catalogueRepository.findById(1).get().getStock().stream().filter(stock -> stock.getQuantity() > 0);
+    }
 }
